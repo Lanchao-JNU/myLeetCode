843812ed800654e0f3186e82baeac7d123a13c15
zhanglanchao <806866316@qq.com>
2021-03-04 16:57:28 +0800

    Opt:topics

--- src/main/java/Topic/Test/TopicsTest.java
+++ src/main/java/Topic/Test/TopicsTest.java
@@ -12,6 +12,17 @@ public class TopicsTest {
         ArrayList <Object> list= node.ListNodetoArrayList(res);
         System.out.println(list);
 
+        String DaySql = "SELECT\n" +
+                "month_time AS dateTime\n" +
+                ",app_name as appName\n"+
+                ",sum(regist_user_new) AS registerUserNum\n" +
+                ",if(sum(regist_user_new)=0,0,round(100*sum(regist_user_2month)/sum(regist_user_new),2))  AS registerUser2PeriodRate\n" +
+                ",if(sum(regist_user_new)=0,0,round(100*sum(regist_user_3month)/sum(regist_user_new),2))  AS registerUser3PeriodRate\n" +
+                ",if(sum(regist_user_new)=0,0,round(100*sum(regist_user_4month)/sum(regist_user_new),2)) AS registerUser4PeriodRate\n" +
+                ",if(sum(regist_user_new)=0,0,round(100*sum(regist_user_5month)/sum(regist_user_new),2))  AS registerUser5PeriodRate\n" +
+                ",if(sum(regist_user_new)=0,0,round(100*sum(regist_user_6month)/sum(regist_user_new),2))  AS registerUser6PeriodRate\n" +
+                "from dm_app_ad_login_info_month\n";
+        System.out.println(DaySql);
     }
 
 }
