c9e16390f74b4f3e565220739b2995121e6e2579
zhanglanchao <806866316@qq.com>
2021-03-19 14:04:50 +0800

    Opt:剑指offer 26

---
+++ src/main/java/Topic/ListNode/isSubStructure.java
@@ -0,0 +1,46 @@
+package Topic.ListNode;
+
+import util.TreeNode;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: zhanglanchao
+ * \* Date: 2021/3/19
+ * \* Time: 1:07 下午
+ * \* Description:
+ * \
+ */
+public class isSubStructure {
+        public boolean isSubStructure(TreeNode A, TreeNode B) {
+
+            if (B==null || A==null) return false;
+            return  isSubStructure(A.left,B) || isSubStructure(A.right,B) || isLikeTree(A,B);
+        }
+
+        public boolean isLikeTree(TreeNode A,TreeNode B){
+
+            if (A==null && B==null){
+                return true;
+            }
+            if (A==null && B!=null){
+                return false;
+            }
+            if (A!=null && B==null){
+                return true;
+            }
+            return A.val==B.val && isLikeTree(A.left,B.left) && isLikeTree(A.right,B.right);
+        }
+    }
+
+//    public boolean isLikeTree(TreeNode A,TreeNode B){
+//        if (A==null && B==null){
+//            return true;
+//        }
+//        if (A==null && B!=null){
+//            return false;
+//        }
+//        if (A!=null && B==null){
+//            return false;
+//        }
+//        return A.val==B.val && isSameTree(A.left,B.left) && isSameTree(A.right,B.right);
+//    }
